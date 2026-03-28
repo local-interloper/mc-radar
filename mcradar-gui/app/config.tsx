67fb8e49e0f915940e519c9ae12a95359e419bbd
local-interloper <marinoljubetic8@gmail.com>
2026-03-28 03:59:09 +0100

    Updated env vars

--- mcradar-gui/app/config.tsx
+++ mcradar-gui/app/config.tsx
@@ -1,1 +1,1 @@
-export const API_URL = `${process.env.APP_API_PROTOCOL}://${process.env.APP_API_URL}/api`;
+export const API_URL = `${import.meta.env.VITE_API_PROTOCOL}://${import.meta.env.VITE_API_URL}/api`;
